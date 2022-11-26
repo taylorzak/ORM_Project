19b0a550a15f0dff4473f706e56f05466b8c6640
taylorzak <taylorzak@hotmail.com>
2022-11-26 12:09:15 -0700

    new files

---
+++ Develop/routes/api/tag-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', (req, res) => {
+  res.send("this is a tag route")
+  // find all tags
+  // be sure to include its associated Product data
+});
+
+router.get('/:id', (req, res) => {
+  // find a single tag by its `id`
+  // be sure to include its associated Product data
+});
+
+router.post('/', (req, res) => {
+  console.log("new tag name: ",req.body)
+  res.send("success")
+  // create a new tag
+});
+
+router.put('/:id', (req, res) => {
+  // update a tag's name by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete on tag by its `id` value
+});
+
+module.exports = router;
